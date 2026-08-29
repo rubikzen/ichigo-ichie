38bea7f8a61f280bd09925f158b64d4bf4cd928a
rubikzen <rubikzen@gmail.com>
2026-08-29 19:02:20 +0200

    V489 mount traffic analytics collector

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -3,8 +3,18 @@
 import { LanguageProvider } from "./LanguageProvider";
 import { CartProvider } from "./CartProvider";
 import { SiteSettingsProvider } from "./SiteSettingsProvider";
+import { VercelTrafficAnalytics } from "./VercelTrafficAnalytics";
 import type { SiteSettings } from "@/lib/settings";
 
 export function Providers({ children, siteSettings }: { children: React.ReactNode; siteSettings: SiteSettings }) {
-  return <LanguageProvider><SiteSettingsProvider initialSettings={siteSettings}><CartProvider>{children}</CartProvider></SiteSettingsProvider></LanguageProvider>;
+  return (
+    <LanguageProvider>
+      <SiteSettingsProvider initialSettings={siteSettings}>
+        <CartProvider>
+          {children}
+          <VercelTrafficAnalytics />
+        </CartProvider>
+      </SiteSettingsProvider>
+    </LanguageProvider>
+  );
 }
